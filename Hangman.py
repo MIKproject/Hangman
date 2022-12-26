ccb8f534489ce0630f54142013d900f4e30346fa
MIK <trixa1@mail.ru>
2022-12-26 19:44:51 +0300

    Add full and correct code 'Hangman'

--- Hangman.py
+++ Hangman.py
@@ -1,6 +1,14 @@
 from random import *
 
-word_list = ['слово', 'раз', 'работа']
+word_list = ['слово', 'работа', 'КЛЮЧ', 'КНИГА', 'ЕНОТ', 'МАШИНКА', 'КОРОВА', 'ТЕЛЕЖКА', 'ШЛЕМ', 'КНОПКА', 'ШНУР', 'ЧЕРНЫЙ',
+'ВЛАСТЕЛИН', 'СКАЙП', 'ДУБ', 'ЧАСЫ', 'ТРУБА', 'ЕЛКА', 'ИНСТИТУТ', 'КОРОБКА', 'ТАБЛИЧКА', 'ВОДА', 'СКОВОРОДА',
+'МНОГОНОЖКА', 'ЕВРЕЙ', 'ТЕРМИТ', 'КАЧЕК', 'РУЛОН', 'МАГНИТОФОН', 'НОГА', 'СЛОН', 'МИКРОВОЛНОВКА', 'ТОРТ', 'МАК',
+'ДЫМ', 'ЧАЙКА', 'ВАЛЕТ', 'ПЛИНТУС', 'ШАПКА', 'ДИНОЗАВР', 'ТОРШЕР', 'БАЛАЛАЙКА', 'БАНКА', 'ЯХТА', 'ОВЦА', 'БАНАН',
+'ДУБ', 'АНИМЕ', 'РАДУГА', 'БУКВА', 'ВЕЛОСИПЕД', 'БАНДЖО', 'ГОЛУБЬ', 'ВИНТОВКА', 'КУБОК', 'ЖАСМИН', 'ТЕЛЕФОН',
+'АНДРОИД', 'ГОРА', 'ХАЛАТ', 'ЖЕТОН', 'ОБОД', 'МЫЛО', 'ЙОГ', 'ШИШКА', 'ДОЛЛАР', 'КОЛОНКА', 'КУБИК', 'ОМАР',
+'РАКЕТА', 'МОРКОВКА', 'ЗЕРКАЛО', 'МОЛОТ', 'ВОЗДУХ', 'ЗМЕЙ', 'ЁЖ', 'ПАЛЬМА', 'МАСЛО', 'ДИДЖЕЙ', 'МЕШОК', 'ТЮБИК',
+'МОЗГ', 'ПОЕЗД', 'РОЗЕТКА', 'ПАРАШЮТИСТ', 'БЕЛКА', 'ШПРОТЫ', 'САМОСВАЛ', 'ПАЗЛ', 'БУТЫЛКА', 'КРЕМЛЬ', 'ПИЦЦА',
+'МАКАРОНЫ', 'КОВЕР', 'ЗУБЫ', 'ЯРЛЫК', 'КАШАЛОТ', 'МАРС', 'ШАКАЛ', 'ПОМАДА', 'ДЖИП']
 
 
 def get_word(word):
@@ -83,47 +91,73 @@ def display_hangman(tries):
 
 
 def play(word):
-    word = get_word(word_list)
     word_completion = '_' * len(word)  # строка, содержащая символы _ на каждую букву задуманного слова
     guessed = False  # сигнальная метка
     guessed_letters = []  # список уже названных букв
     guessed_words = []  # список уже названных слов
-    tries = 6
-    print()
+    tries = 6 # количество попыток
+    
     print('Давайте играть в угадайку слов!')
+    print(f'Виселица: {display_hangman(tries)}')
+    print(f'Загаданное слово: {word_completion}')
+    
     while tries:
-        print(word_completion)
         print(f'Список уже названных букв {guessed_letters}')
         print(f'Список уже названных слов {guessed_words}')
-        letter = input('Введите букву или слово целиком, если уже догадались:_').upper()
-        if len(letter) == 1:
-            guessed_letters.append(letter)
-        if len(letter) > 1:
-            guessed_words.append(letter)
-        if letter == word:
-            return 'Поздравляем, вы угадали слово! Вы победили!'
+        letter = input('Введите букву или слово целиком: ').upper()
         if not letter.isalpha():
-            print('Введите символ, являющийся буквой')
+            print('Введите букву или слово, попробуйте еще раз')
             continue
-        if letter in word_completion:
-            print('Эту букву Вы уже отгадали')
+        if letter in guessed_letters or letter in guessed_words:
+            print('Вы это уже вводили')
             continue
-        if letter in word:
-            for i in range(len(word)):
-                if word[i] == letter:
-                    word_completion = word_completion[:i] + letter + word_completion[i + 1:]
-                    if word_completion == word:
-                        return 'Поздравляем, вы угадали слово! Вы победили!'
+
+        if len(letter) > 1:
+            if letter == word or guessed:
+                print('Поздравляем, вы угадали слово! Вы победили!')
+                print(f'Загаданное слово: {word}')
+                break
+            elif letter != word:
+                print(f'Не верное слово! Осталось попыток {tries - 1}')
+                tries -= 1
+                print(f'Виселица: {display_hangman(tries)}')
+                guessed_words.append(letter)
+            
+        if len(letter) == 1:
+            if letter in word:
+                word_completion_as_list = list(word_completion)
+                indices = [i for i in range(len(word)) if word[i] == letter]
+                for index in indices:
+                    word_completion_as_list[index] = letter
+                word_completion = ''.join(word_completion_as_list)
+                guessed_letters.append(letter)
+                print('Вы угадали букву!')
+                print(word_completion)
+                if '_' not in word_completion:
+                    print('Поздравляем, вы угадали слово! Вы победили!')
+                    print(f'Загаданное слово: {word}')
+                    break
+                    
+            elif letter not in word:
+                print(f'Такой буквы нет! Осталось попыток {tries - 1}')
+                tries -= 1
+                print(f'Виселица: {display_hangman(tries)}')
+                guessed_letters.append(letter)
         
-        else:
-            if len(letter) == 1:
-                print('Такой буквы нет')
-            if len(letter) > 1:
-                print('Не верное слово!')
-            tries -= 1
-            print(display_hangman(tries))
         if tries == 0:
-            return f'Вы проиграли! Правильный ответ: {word}'
+            print(f'Виселица: {display_hangman(tries)}')
+            print(f'Вы проиграли! Правильный ответ: {word}')
+            break
 
 
-print(play(get_word(word_list)))
+def hangman(word):
+    wanna_play = int(input('Играть снова? (да-1/0-нет): '))
+    while wanna_play == 1:
+        word = get_word(word_list)
+        play(word)
+        wanna_play = int(input('Играть снова? (да-1/0-нет): '))
+        print()
+    return 'Возвращайтесь снова, до свидания!'
+
+
+print(hangman(play(get_word(word_list))))
